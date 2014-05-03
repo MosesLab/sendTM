6e9e180ea480ad3dd97f871946f7416a86803779
birdie <birdie@teslaCoil>
2014-05-03 09:50:45 -0600

    changed idle around in attempt to fix nonblocking read mode

--- sendTM/sendTM.c
+++ sendTM/sendTM.c
@@ -211,8 +211,8 @@ int main (int argc, char ** argv){
 	}
 
 	/* set transmit idle pattern (sent between frames) */
-	//idle = HDLC_TXIDLE_ALT_ZEROS_ONES;
-        idle = HDLC_TXIDLE_CUSTOM_8 + 0xff;
+	idle = HDLC_TXIDLE_ALT_ZEROS_ONES;
+        //idle = HDLC_TXIDLE_CUSTOM_8 + 0xff;
 	rc = ioctl(fd, MGSL_IOCSTXIDLE, idle);
 	if (rc < 0) {
 		printf("ioctl(MGSL_IOCSTXIDLE) error=%d %s\n",
